798a3d12d6acaddad87637f71d6f4f0b4446fe06
chrissteven96 <christogram96s@gmail.com>
2026-05-08 01:35:49 -0500

    hero_logo

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -7,10 +7,16 @@ const Hero = () => {
             <div className={styles.hero}>
                 <div className={styles.heroContent}>
                    <div className={styles.heroText}>
-                    <h1>Hero</h1>
+                    <span className={styles.heroTextSpanMin}>+10 años haciendo marcas visibles</span>
+                    <br />
+                    <span className={styles.heroTextSpan}>Hacemos que <br/>
+                        tu marca <br/>
+                        <span className={styles.heroTextRed}>grite <br/></span>
+                        en la calle.</span> <br />
+                        <p className={styles.heroTextSpanDown}>RE Design es rotulación y publicidad con trayectoria. Personal capacitado, materiales premium y maquinaria de última tecnología al servicio de tu marca.</p>
                    </div>
                    <div className={styles.heroLogo}>
-                    <img src="/log_redesign.png" alt="Logo" className={styles.logoImage}/>
+                    <img src="/logo_hero.png" alt="Logo" className={styles.logoImage}/>
                    </div>
                 </div>
             </div>
