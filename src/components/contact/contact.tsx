88abb15ced23db5ba71e4925dba2e340e91e6e12
chrissteven96 <christogram96s@gmail.com>
2026-06-25 11:02:29 -0500

    whatsapp

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './contact.module.css'
-import { FaPhone, FaEnvelope } from 'react-icons/fa'
+import { FaEnvelope, FaWhatsapp } from 'react-icons/fa'
 
 const Contact = () => {
     return (
@@ -8,13 +8,13 @@ const Contact = () => {
         <div className={styles.contact}>
             <div className={styles.contactContent}>
                 <div className={styles.contactText}>
-                    <h1>Publicidad <br /> estratégica <span>a tu alcance</span></h1>
+                    <h1>Publicidad estratégica <br /> <span>a tu alcance</span></h1>
                     <p>Cotiza tu proyecto con nosotros. Te respondemos en menos de 24 horas.</p>
                 </div>
 
             <div className={styles.buttonSpace}>
-                <a href="https://wa.me/593962725180?text=Hola%20R2A%20Publicidad%20quiero%20cotizar%20uno%20de%20sus%20servicios" target="_blank" rel="noopener noreferrer" className={styles.button1}> <FaPhone /> 096 272 5180</a>
-                <a href="mailto:r2apublicidad@hotmail.com" target="_blank" rel="noopener noreferrer" className={styles.button2}> <FaEnvelope /> Escríbenos</a>
+                <a href="https://wa.me/593962725180?text=Hola%20R2A%20Publicidad%20quiero%20cotizar%20uno%20de%20sus%20servicios" target="_blank" rel="noopener noreferrer" className={styles.button1}> <FaWhatsapp size={30} /> 096 272 5180</a>
+                <a href="mailto:r2apublicidad@hotmail.com" target="_blank" rel="noopener noreferrer" className={styles.button2}> <FaEnvelope size={20} /> Escríbenos</a>
 
             </div>
 
